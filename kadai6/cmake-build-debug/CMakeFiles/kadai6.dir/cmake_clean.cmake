012faca9632fda2bbd157a043e2789d37e9c935e
ToshimiGunji <toshimi0218@icloud.com>
2017-09-26 17:53:57 +0900

    Add files via upload

---
+++ kadai6/cmake-build-debug/CMakeFiles/kadai6.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kadai6.dir/main.c.obj"
+  "kadai6.pdb"
+  "kadai6.exe"
+  "kadai6.exe.manifest"
+  "libkadai6.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/kadai6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ kadai6/cmake-build-debug/CMakeFiles/kadai6.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for kadai6.
+# This may be replaced when dependencies are built.
