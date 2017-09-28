e9a52a296954feb9aa0fdb7b932dadb45ef47414
Toshimi <toshimi0218@icloud.com>
2017-09-28 10:44:55 +0900

    Nothing

---
+++ kadai7/cmake-build-debug/CMakeFiles/kadai7.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kadai7.dir/main.c.obj"
+  "kadai7.pdb"
+  "kadai7.exe"
+  "kadai7.exe.manifest"
+  "libkadai7.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/kadai7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
